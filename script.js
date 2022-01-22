1bd1cde7941573d24d7875968902678799a66f00
Nicolas Danciu <nixrom1@gmail.com>
2022-01-22 14:49:05 -0500

    my first try at a password generator

---
+++ script.js
@@ -0,0 +1,24 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var passwordTypeLong = prompt("What is your desired password length? ");
+var passwordType = prompt("What is your desired character type? ");
+var passwordType = prompt("What are the characters you do not want present? ");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+   function generatePassword() {
+    var length = 12
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        returnValue = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        returnValue += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return returnValue;
+}
+}
+
+
+generateBtn.addEventListener("click", writePassword);
